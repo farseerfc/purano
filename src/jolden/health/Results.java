f97494981ccb3cc41934b3eb10d690a97742e0d7
farseerfc <farseerfc>
2014-06-18 05:31:49 +0900

    add jolden test suite

---
+++ src/jolden/health/Results.java
@@ -0,0 +1,12 @@
+package jolden.health;
+
+
+/**
+ * A class to contains the results from the health care simulation.
+ **/
+class Results
+{
+  float totalPatients;
+  float totalTime;
+  float totalHospitals;
+}
---
+++ src/jolden/voronoi/EdgePair.java
@@ -0,0 +1,29 @@
+package jolden.voronoi;
+
+import java.io.*;
+
+/**
+ * A class that represents an edge pair
+ **/
+public class EdgePair 
+{
+  Edge left;
+  Edge right;
+  
+  public EdgePair(Edge l, Edge r)
+  {
+    left = l;
+    right = r;
+  }
+
+  public Edge getLeft()
+  {
+    return left;
+  }
+
+  public Edge getRight() 
+  {
+    return right;
+  }
+
+}  
